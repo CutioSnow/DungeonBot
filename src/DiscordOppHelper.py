089eb9f63257a733916531257f6b296c27a25699
Steve <sshort95@gmailt.com>
2022-11-10 14:25:54 -0500

    Addition of embedGen

--- src/DiscordOppHelper.py
+++ src/DiscordOppHelper.py
@@ -8,9 +8,28 @@ class DiscordOppHelper:
     def __init__(self) -> None:
         pass
 
-    def embedGenUserDisplay(auth:discord.Member, title:str, msg:str, color:int) -> discord.Embed:
+    def embedGen(title:str, msg:str, color:int=0) -> discord.Embed:
         """
-        The embedGenUserDisplay method is a static method used to generate a
+        The embedGen method is a static method used to generate a discord embed
+        object with set title, message, and boarder color(discord default color if
+        none is set).
+
+        Parameters:
+            title (str): title of embeded message
+            msg (str): contents of embeded message
+            color (int): integer value representing embed boarder colour. Discord
+                         default if none is set
+        
+        Returns:
+            embed (discord.Embed): Formated discord Embed object
+        """
+        #Creates and returns discord embed object
+        embed:discord.Embed = discord.Embed(title=title,description=msg,colour=color)
+        return embed
+
+    def embedGenUserDisplay(auth:discord.Member, title:str, msg:str, color:int=0) -> discord.Embed:
+        """
+        The embedGenUserDisplay method is a sister used to generate a
         discord embed object with a set username and image. Requires a discord 
         Member object to find author image url and display name.
 
@@ -18,7 +37,8 @@ class DiscordOppHelper:
             auth (discord.member): Author of discord client message
             title (str): title of embeded message
             msg (str): contents of embeded message
-            color (int): integer value representing embed boarder colour
+            color (int): integer value representing embed boarder colour. Discord
+                         default if none is set
         
         Returns:
             embed (discord.Embed): Formated discord Embed object
