7737a15dedd6c87db9c58ff1e6d9f5402225a463
Steve <sshort95@gmailt.com>
2022-11-17 17:38:55 -0500

    Weighted Random

--- src/RandomNumberGenerator.py
+++ src/RandomNumberGenerator.py
@@ -1,4 +1,5 @@
-from numpy import random, ndarray
+from numpy import random, ndarray, array
+import random as ran
 
 class RandomNumberGenerator:
     '''
@@ -24,9 +25,30 @@ class RandomNumberGenerator:
             set: Set of random Integers contained in a NumPy.ndarray
         '''
         #Initialize the Default NumPy random number generator
+        #TODO: Write documentation https://www.geeksforgeeks.org/python-lists-vs-numpy-arrays/
         rng = random.default_rng()
         #Generates a set of random Integers of a defined size
-        set = rng.integers(low=low,high=high+1,size=size)
+        set:ndarray = rng.integers(low=low,high=high+1,size=size)
         #returns the set of generated Integers
         return set
+
+    def randomIntWeightedHigh(low:int,high:int,size:int) -> ndarray:
+        '''
+        The randomIntWeightedHigh method generates a set of random integers in 
+        a specified range (inclusive) and size weighted to prefer higher valued
+        integers (i.e. low=1 and high=20 will favor values >10).
+
+        Parameters:
+            low: Lowest value in the generated range (inclusive)
+            high: Highest Value in the generated range (inclusive)
+            size: Non-zero value for the number of generated Integers
+        
+        Returns:
+            set: Weighted set of random Integers in a NumPy.ndarray
+        '''
+        #Initializes list to contain generated random numbers
+        
+
+    def weightGenerate():
+        ''''''
 
